29a81f22fc2c1e0dcd7ff2b6161eaa91c4a403b8
unknown <commandodev@protonmail.com>
2021-03-10 21:14:11 +0100

    wrote initial assembly code for task 1

--- 1.asm
+++ 1.asm
@@ -1,7 +1,24 @@
-data: 
-   input: .asciiz "THIS 123 984 STRING"
+.data
+input: .space 256
    
-text: 
-   li $t0, 1
-   
-   
+.text
+main: 
+   	la $a0, input
+   	li $a1, 256,
+   	li $v0, 8
+   	syscall
+
+	la $t0, input
+loop:
+	lbu $t1, ($t0)
+	beqz $t1 endloop
+	blt $t1, '0', loop
+	bgt $t1, '9', loop
+	subu $t2, $t2, $t2 
+	addi $t2, $t2, '9'
+	addi $t2, $t2, '0'
+	lbu $t1, $t2 
+	lbu $t0, $t1
+	addi $t0, 1
+	
+endloop:
